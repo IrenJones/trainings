f81d3d51ff8b773f4c3311379d38025d44f8b0d9
IrenJones <codovoeprostranstvo@gmail.com>
2024-04-09 23:58:24 -0700

    [l] add solutions for some problems

--- src/main/java/SparkHello.java
+++ src/main/java/SparkHello.java
@@ -1,10 +1,17 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class SparkHello {
+
+	private static final Pattern SPACE = Pattern.compile(" ");
+
 	public static void main(String[] args) {
 
 		SparkConf sparkConf = new SparkConf()
@@ -17,5 +24,18 @@ public class SparkHello {
 		nums.map(x -> x * x)
 				.collect()
 				.forEach(System.out::println);
+
+
+		JavaRDD<String> lines = ctx.textFile(args[0], 1);
+
+		JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
+		JavaPairRDD<String, Integer> ones = words.mapToPair(word -> new Tuple2<>(word, 1));
+		JavaPairRDD<String, Integer> counts = ones.reduceByKey(Integer::sum);
+
+		List<Tuple2<String, Integer>> output = counts.collect();
+		for (Tuple2<?, ?> tuple : output) {
+			System.out.println(tuple._1() + ": " + tuple._2());
+		}
+		ctx.stop();
 	}
 }
